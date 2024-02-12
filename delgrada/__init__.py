f6fa563cdf53d52bd650b37d99f4e557241fae2f
perceptronv <neutrinovs@gmail.com>
2024-02-12 17:09:18 +0800

    Moved files & imports around

--- delgrada/__init__.py
+++ delgrada/__init__.py
@@ -3,6 +3,9 @@ from delgrada.tensor import Tensor
 from delgrada.tensops import shape, transpose, full
 from delgrada.tensops import exp, log, sin, cos
 
-from delgrada.dtype import *
-
 from delgrada.scalar import Scalar
+
+# Re-use numpy dtypes
+from numpy import integer, short, longlong
+from numpy import half, single, double, float16, float32, float64
+from numpy import csingle, cdouble, complex64, complex128
--- delgrada/dtype.py
+++
@@ -1,3 +0,0 @@
-from numpy import integer, short, longlong
-from numpy import half, single, double, float16, float32, float64
-from numpy import csingle, cdouble, complex64, complex128
